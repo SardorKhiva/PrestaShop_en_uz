0a9d7e6aadc4df3b2c3b809dc31836591789ce30
Leader <leaderkhiva@gmail.com>
2025-07-11 09:18:21 +0500

    mcpserver.py o'chirildi

--- mcpserver.py
+++
@@ -1,8 +0,0 @@
-from mcp.server.fastmcp import FastMCP
-from tools.translator import translator  # ваш модуль перевода
-
-mcp = FastMCP("translator-agent")
-translator.register(mcp)
-
-if __name__ == "__main__":
-    mcp.run()
